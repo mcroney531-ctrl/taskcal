f6ba7b81ba481ccbe66d65a9b5e4391f2a4cf595
mcroney531-ctrl <mcroney531@gmail.com>
2026-05-29 23:55:13 -0400

    initial commit

---
+++ app/src/main/java/com/rone/taskapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.rone.taskapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/task-calendar.html")
+        setContentView(webView)
+    }
+}
